d8141550a7d921d6cb2d6113a6af4b5e6e5f0a4d
psbrunosouza <psbrunosouza@gmail.com>
2021-07-06 22:02:11 -0300

    refatoring workspaces controller to improve error handling

--- src/controllers/workspace.controller.ts
+++ src/controllers/workspace.controller.ts
@@ -8,18 +8,30 @@ export class WorkspaceController {
   async index(request: Request, response: Response) {
     const workspaceRepository: WorkspaceRepository =
       getCustomRepository(WorkspaceRepository);
-    const [workspaces, count] = await workspaceRepository.findAndCount({
-      withDeleted: false,
-    });
 
-    return response.status(200).json({
-      response: {
-        data: { workspaces, count },
-        errors: [],
-        status: 201,
-        success: true,
-      },
-    });
+    try {
+      const [workspaces, count] = await workspaceRepository.findAndCount({
+        withDeleted: false,
+      });
+
+      return response.status(200).json({
+        response: {
+          data: { workspaces, count },
+          errors: [],
+          status: 201,
+          success: true,
+        },
+      });
+    } catch (err) {
+      return response.status(500).json({
+        response: {
+          data: {},
+          errors: ["internal server error", err.message],
+          status: 500,
+          success: false,
+        },
+      });
+    }
   }
 
   async store(request: Request, response: Response) {
@@ -28,42 +40,53 @@ export class WorkspaceController {
       getCustomRepository(WorkspaceRepository);
     const userRepository: UserRepository = getCustomRepository(UserRepository);
 
-    const user = await userRepository.findOne({
-      where: { id: workspace.userId },
-    });
-
-    if (!user) {
-      return response.status(404).json({
+    try{
+      const user = await userRepository.findOne({
+        where: { id: workspace.userId },
+      });
+  
+      if (!user) {
+        return response.status(404).json({
+          response: {
+            data: {},
+            errors: ["user does not exists"],
+            status: 404,
+            success: false,
+          },
+        });
+      }
+  
+      if (!workspace.name) {
+        return response.status(422).json({
+          response: {
+            data: {},
+            errors: ["entity 'name' cannot be empty"],
+            status: 422,
+            success: false,
+          },
+        });
+      }
+  
+      await workspaceRepository.save(workspace);
+  
+      return response.status(201).json({
+        response: {
+          data: { workspace },
+          errors: [],
+          status: 201,
+          success: true,
+        },
+      });
+    }catch(err){
+      return response.status(500).json({
         response: {
           data: {},
-          errors: ["user does not exists"],
-          status: 404,
+          errors: ["internal server error", err.message],
+          status: 500,
           success: false,
         },
       });
     }
-
-    if (!workspace.name) {
-      return response.status(422).json({
-        response: {
-          data: {},
-          errors: ["entity 'name' cannot be empty"],
-          status: 422,
-          success: false,
-        },
-      });
-    }
-
-    await workspaceRepository.save(workspace);
-
-    return response.status(201).json({
-      response: {
-        data: { workspace },
-        errors: [],
-        status: 201,
-        success: true,
-      },
-    });
   }
 
   async edit(request: Request, response: Response) {
@@ -72,29 +95,40 @@ export class WorkspaceController {
     const workspaceRepository: WorkspaceRepository =
       getCustomRepository(WorkspaceRepository);
 
-    const hasWorkspace = await workspaceRepository.findOne(id);
+    try{
+      const hasWorkspace = await workspaceRepository.findOne(id);
 
-    if (!hasWorkspace) {
-      return response.status(404).json({
+      if (!hasWorkspace) {
+        return response.status(404).json({
+          response: {
+            data: {},
+            errors: ["workspace was not found"],
+            status: 404,
+            success: false,
+          },
+        });
+      }
+  
+      await workspaceRepository.save({ ...hasWorkspace, ...workspace });
+  
+      return response.status(200).json({
+        response: {
+          data: { workspace: { ...hasWorkspace, ...workspace } },
+          errors: [],
+          status: 200,
+          success: true,
+        },
+      });
+    }catch(err){
+      return response.status(500).json({
         response: {
           data: {},
-          errors: ["workspace was not found"],
-          status: 404,
+          errors: ["internal server error", err.message],
+          status: 500,
           success: false,
         },
       });
     }
-
-    await workspaceRepository.save({ ...hasWorkspace, ...workspace });
-
-    return response.status(200).json({
-      response: {
-        data: { workspace: { ...hasWorkspace, ...workspace } },
-        errors: [],
-        status: 200,
-        success: true,
-      },
-    });
   }
 
   async delete(request: Request, response: Response) {

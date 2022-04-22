91e460dbd11a40b588d436f4b5b4d3c3b9eda67e
psbrunosouza <psbrunosouza@gmail.com>
2022-04-22 08:22:11 -0300

    build: change configs

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,7 @@ export class ApiConfigurations {
   baseUrl;
 
   constructor() {
-    this.port = process.env.API_REST_PORT ?? 3333;
+    this.port = process.env.PORT ?? 3333;
     this.baseUrl = process.env.API_BASE_URL ?? 'v1/arbol';
   }
 }

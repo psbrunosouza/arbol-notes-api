e51f52d73670a761e61527b45a422bf5ad27dcd9
Bruno <psbrunosouza@gmail.com>
2022-07-25 22:08:39 -0300

    feat(server): add cors config

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,13 +9,26 @@ import { errors as validationErrorsHandler } from 'celebrate';
 import { PrismaBranchRepository } from '@modules/branch/infra/prisma/repositories/PrismaBranchRepository';
 import api from '@config/api';
 
+const whitelist = ['http://localhost:4200', 'https://arbol-notes.vercel.app'];
+
 const apiConfig = api();
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(<string>origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }),
+);
 
 app.use(express.json());
 
